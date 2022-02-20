f1e1a50b1224525aa57a2d4ac370fe43c0e03d71
“heythereimchris” <chrissanfilippo10@gmail.com>
2022-02-19 20:44:19 -0800

    landing copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,11 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1>PWA DEMO APP</h1>
+
+        {/* <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </h1> */}
 
         <p className={styles.description}>
           Get started by editing{' '}
