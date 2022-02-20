aa053a5e6f57b7a33f67a4422acd03f01167c8a4
“heythereimchris” <chrissanfilippo10@gmail.com>
2022-02-19 20:53:14 -0800

    merge all changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,12 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <h1> Installable PWA</h1>
+          {/* Welcome to <a href="https://nextjs.org">Next.js!</a> */}
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+
         </p>
 
         <div className={styles.grid}>
